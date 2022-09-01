416faffccbc5c02deb1a8ad5ff1c82c1e0c8bf00
mhema <mhema@192.168.7.120>
2022-09-01 22:33:38 +0200

    added conditions to show sickness if patient is sick, and to show healthy if sick patient is treated with correct medicine

--- src/main/java/hospital/Patient.java
+++ src/main/java/hospital/Patient.java
@@ -4,7 +4,8 @@ public class Patient {
 
 	private String name;
 	private String getSickness;
-
+	protected boolean isSick = true;
+	
 	protected Patient(String name, String getSickness) {
 		this.name = name;
 		this.getSickness = getSickness;
@@ -18,19 +19,20 @@ public class Patient {
 		return name;
 	}
 
-	protected boolean isSick() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
 	protected String getSickness() {
-		// TODO Auto-generated method stub
 		return getSickness;
 	}
 
-	protected void takeMedication(Medicine medicine) {
-		// TODO Auto-generated method stub
-		
+	protected boolean isSick() {
+		if(getSickness == null ) {
+			isSick = false;
+		}
+		return isSick;
 	}
-
+	
+	protected void takeMedication(Medicine medicine) {
+		if(medicine.getTreatmentName().equals(getSickness)) {
+			isSick = false;
+		}
+	}	
 }
