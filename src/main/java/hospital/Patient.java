05f5fe1fb316ba8fabec76666ec5654ee56b51bd
mhema <mhema@192.168.7.120>
2022-09-01 17:06:30 +0200

    cloned labb3 (hospital) repo, renamed project to hospital (and fixed some problems after renaming), auto-generated two constructors (for sick patients and for healthy patients), auto-generated four methods (getName, isSick, getSickness and takeMedication)

---
+++ src/main/java/hospital/Patient.java
@@ -0,0 +1,33 @@
+package hospital;
+
+public class Patient {
+
+	public Patient(String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Patient(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isSick() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getSickness() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void takeMedication(Medicine medicine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
